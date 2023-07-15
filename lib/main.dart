e9c45cee9d23f75282972c80344757a3f1816f72
poer2424 <wogur4612@naver.com>
2023-07-16 00:07:43 +0900

    이미지 에셋 추가, main.dart의 메인페이지 수정, pubspec.yaml에 이미지에셋 경로 추가

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -23,10 +24,76 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _money = 0;
+  int _electricity = 0;
+  int _co2 = 0;
+
+  void _set_money(){
+    setState(() {
+      _money = _money + 100;        //상수는 그날 절약 금액에 대한 변수로 후에 대체
+    });
+  }
+
+  void _set_electricity(){
+    setState(() {
+      _electricity = _electricity + 100;        //상수는 그날 절약 전력량에 대한 변수로 후에 대체
+    });
+  }
+
+  void _set_co2(){
+    setState(() {
+      _co2 = _co2 + 100;            //상수는 그날 절약 이산화탄소량에 대한 변수로 후에 대체
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      body: Row(
+        children: [
+          Column(
+            children: [
+              Text('누적 절약 금액', style: Theme.of(context).textTheme.headlineLarge,),
+              InkWell(
+                onTap: (){
+                },
+                child: Image.asset('assets/img/piggy_bank.png'),
+              ),
+              Text('$_money 원', style: Theme.of(context).textTheme.headlineMedium,),
+            ],
+          ),
+          Column(
+
+          ),
+          Column(
+
+          )
+        ],
+      )
     );
   }
 }
+
+//
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SizedBox(
+//         width: MediaQuery.of(context).size.width,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: [
+//             InkWell(
+//               onTap: (){
+//               },
+//               child: Image.asset('assets/img/piggy_bank.png'),
+//             )
+//           ],
+//         )
+//       )
+//     );
+//   }
+// }
