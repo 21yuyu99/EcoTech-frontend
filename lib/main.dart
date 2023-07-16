50d03a3ee6e56ffbdf65ae90e2d5f363dfd44c09
poer2424 <wogur4612@naver.com>
2023-07-16 23:38:09 +0900

    디자인 완성

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _HomeState extends State<Home> {
   int _money = 0;
   int _electricity = 0;
   int _co2 = 0;
+  int _level = 1;
 
   void _set_money(){
     setState(() {
@@ -46,12 +47,18 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void _set_level(){
+    setState(() {
+      _level = _level + 1;          // 레벨이 올라갈 때 마다 setstate
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          const SizedBox(height: 150),
+          const SizedBox(height: 200),
           Row(
             children: [
               const SizedBox(width: 10,),
@@ -92,7 +99,7 @@ class _HomeState extends State<Home> {
                             },
                             child: Image.asset('assets/img/cloud_co2.png', width: 100, height: 100,),
                           ),
-                          Text('$_co2 kwh', style: Theme.of(context).textTheme.headlineSmall,)
+                          Text('$_co2 gCO2e', style: Theme.of(context).textTheme.headlineSmall,)
                         ],
                       ),
                     ],
@@ -101,7 +108,16 @@ class _HomeState extends State<Home> {
               ),
             ],
           ),
-          Image.asset('assets/img/sprout_and_barren_land.png', height: 600, width: 600,)
+          SizedBox(height: 100,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("LV.$_level", style: Theme.of(context).textTheme.headlineMedium,),
+              Padding(padding: EdgeInsets.all(5)),
+              Image.asset('assets/img/notification.png', width: 30, height: 30,)
+            ],
+          ),
+          Image.asset('assets/img/sprout_and_barren_land.png', height: 420, width: 500,)
         ],
       )
 
