7de9ceccf2d95f0a789a06962d49d9ab928fa615
21yuyu99 <yujeong0720@gmail.com>
2023-07-19 00:25:10 +0900

    로그인 페이지 퍼블리싱

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -16,83 +16,111 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body : Container(
-          padding: EdgeInsets.only(top: 40),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Stack(
             children: [
-              Column(
-                children: [
-                  Image.asset('assets/img/yellow_pig.png'),
-                  Text("에코테크",style: TextStyle(
-                      fontFamily: 'gaegu_bold', letterSpacing: 15.0,
-                      fontSize: 40,color: AppColor.primaryColor,fontWeight: FontWeight.w800,height: 1.2),),
-                ],
+              Container(
+                padding: EdgeInsets.only(top: 0),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xffFFF09E),
+                        Colors.white,
+                      ],
+                    )
+                ),
               ),
-              Text("서비스 이용을 위해 로그인이 필요해요",style: TextStyle(
-                fontSize: 20,fontWeight: FontWeight.w500
-              ),),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.07),
-              child: Column(
-                children: [
-                  InkWell(onTap:
-                              () async {
-                                    if (await isKakaoTalkInstalled()) {
-                                    try {
-                                    await UserApi.instance.loginWithKakaoTalk();
-                                    print('카카오톡으로 로그인 성공');
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top:50,left:MediaQuery.of(context).size.width * 0.04,
+                    right: MediaQuery.of(context).size.width * 0.04,
+                    // top : MediaQuery.of(context).size.width * 0.5,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.white,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 45,),
+                      Text("서비스 이용을 위해 로그인이 필요해요",style: TextStyle(
+                          fontSize: 20,fontWeight: FontWeight.w500),),
+                      SizedBox(height: 60,),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.02),
+                        child: Column(
+                          children: [
+                            InkWell(onTap:
+                                () async {
+                              if (await isKakaoTalkInstalled()) {
+                                try {
+                                  await UserApi.instance.loginWithKakaoTalk();
+                                  print('카카오톡으로 로그인 성공');
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => const Home()),
+                                  );
+                                } catch (error) {
+                                  print('카카오톡으로 로그인 실패 $error');
+
+                                  // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+                                  try {
+                                    await UserApi.instance.loginWithKakaoAccount();
+                                    print('카카오계정으로 로그인 성공');
                                     Navigator.push(
                                       context,
                                       MaterialPageRoute(builder: (context) => const Home()),
                                     );
-                                    } catch (error) {
-                                    print('카카오톡으로 로그인 실패 $error');
-
-                                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-                                    try {
-                                      await UserApi.instance.loginWithKakaoAccount();
-                                      print('카카오계정으로 로그인 성공');
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => const Home()),
-                                      );
-                                    } catch (error) {
-                                        print('카카오계정으로 로그인 실패 $error');
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => LoginPage()),
-                                        );
-                                    }
-                                    }
-                                    }
-                                    else {
-                                    try {
-                                        await UserApi.instance.loginWithKakaoAccount();
-                                        print('카카오계정으로 로그인 성공');
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => const Home()),
-                                        );
-                                    }
-                                    catch (error) {
-                                        print('카카오계정으로 로그인 실패 $error');
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => LoginPage()),
-                                        );
-                                    }
-                                    }
-                                  },
-                      child: Image.asset('assets/img/kakao_btn.png'),),
-                  SizedBox(height: 45,),
-                  Image.asset('assets/img/google_btn.png'),
-                ],
-              ),)
-            ],
+                                  } catch (error) {
+                                    print('카카오계정으로 로그인 실패 $error');
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => LoginPage()),
+                                    );
+                                  }
+                                }
+                              }
+                              else {
+                                try {
+                                  await UserApi.instance.loginWithKakaoAccount();
+                                  print('카카오계정으로 로그인 성공');
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => const Home()),
+                                  );
+                                }
+                                catch (error) {
+                                  print('카카오계정으로 로그인 실패 $error');
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => LoginPage()),
+                                  );
+                                }
+                              }
+                            },
+                              child: Image.asset('assets/img/kakao_btn.png'),),
+                            SizedBox(height: 35,),
+                            Image.asset('assets/img/google_btn.png'),
+                          ],
+                        ),)
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(bottom: MediaQuery.of(context).size.height*0.5+100,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  child: Image.asset('assets/img/circle_logo.png',width: 80),
+                ),)//
+                  ],
+                ),
           ),
-        )
-    );
+        );
   }
 }
